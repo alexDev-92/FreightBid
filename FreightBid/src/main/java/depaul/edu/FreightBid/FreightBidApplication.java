b641ba3cee97b7536117262840f9b9e1405bd783
Alejandro Amaya <amayaalex19@gmail.com>
2020-08-02 17:48:45 -0500

    first commit

---
+++ FreightBid/src/main/java/depaul/edu/FreightBid/FreightBidApplication.java
@@ -0,0 +1,13 @@
+package depaul.edu.FreightBid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreightBidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreightBidApplication.class, args);
+	}
+
+}
---
+++ FreightBid/src/test/java/depaul/edu/FreightBid/FreightBidApplicationTests.java
@@ -0,0 +1,13 @@
+package depaul.edu.FreightBid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreightBidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
