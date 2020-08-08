158e758fe1af761408b67cd083552d637494e8eb
Alejandro Amaya <amayaalex19@gmail.com>
2020-08-07 23:30:17 -0500

    added addLane html form and code to controller for it

--- FreightBid/src/main/java/depaul/edu/FreightBid/controller/FreightBidController.java
+++ FreightBid/src/main/java/depaul/edu/FreightBid/controller/FreightBidController.java
@@ -3,7 +3,10 @@ package depaul.edu.FreightBid.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import depaul.edu.FreightBid.model.Lane;
@@ -26,6 +29,27 @@ public class FreightBidController {
 		model.addAttribute("Lanes", laneRepo.findAll());
 		
 		return "lanes/viewLanes";
+		
+		
+	}
+	
+	@RequestMapping(params = "addLane")
+	public String addLane(Model model) {
+		model.addAttribute("newLane", new Lane());
+		return "lanes/addLane";
+		
+	}
+	
+	@PostMapping
+	public String saveLane(@ModelAttribute("newLane") Lane lane, BindingResult binding  ) {
+		if(binding.hasErrors()) {
+			return "lanes/addLane";
+		}
+		
+		laneRepo.save(lane);
+		
+		return "redirect:/freightBid/lanes";
+		
 		
 		
 	}
