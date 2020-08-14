41df35c6b34736b5578a622392514e2e0f277692
Alejandro Amaya <amayaalex19@gmail.com>
2020-08-13 22:43:15 -0500

    deleted old controller, model, repo

--- FreightBid/src/main/java/depaul/edu/model/LaneRepository.java
+++
@@ -1,9 +0,0 @@
-package depaul.edu.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LaneRepository extends JpaRepository<Lane, Long> {
-
-}
