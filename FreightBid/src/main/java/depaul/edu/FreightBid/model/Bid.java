3bc442f73aaabe5e62f98256bd0fe04e7a41add3
Alejandro Amaya <amayaalex19@gmail.com>
2020-08-14 22:53:00 -0500

    added bid and needed classes

---
+++ FreightBid/src/main/java/depaul/edu/FreightBid/model/Bid.java
@@ -0,0 +1,27 @@
+package depaul.edu.FreightBid.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Bid implements Serializable{
+
+	private static final long serialVersionUID = 3L;
+	
+	
+	@Id
+	private long id;
+	
+	
+	private int amount;
+	
+	private Carrier carrier;
+	
+	private Lane lane;
+
+}
---
+++ FreightBid/src/main/java/depaul/edu/FreightBid/model/BidRepository.java
@@ -0,0 +1,11 @@
+package depaul.edu.FreightBid.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface BidRepository extends JpaRepository<Bid, Long>{
+	
+
+}
