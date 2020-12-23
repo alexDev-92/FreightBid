b73e593b22e452122cfa914e137bee6191688c9a
Alejandro Amaya <amayaalex19@gmail.com>
2020-12-23 14:22:51 -0600

    added createBid to service

--- FreightBid/src/main/java/depaul/edu/FreightBid/model/BidRepository.java
+++ FreightBid/src/main/java/depaul/edu/FreightBid/model/BidRepository.java
@@ -1,5 +1,7 @@
 package depaul.edu.FreightBid.model;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import org.springframework.stereotype.Repository;
 public interface BidRepository extends JpaRepository<Bid, Long>{
 	
 
+	List <Bid> findByLane(Lane lane);
 }
--- FreightBid/src/main/java/depaul/edu/FreightBid/service/BidService.java
+++ FreightBid/src/main/java/depaul/edu/FreightBid/service/BidService.java
@@ -8,12 +8,34 @@ import org.springframework.stereotype.Service;
 
 import depaul.edu.FreightBid.model.Bid;
 import depaul.edu.FreightBid.model.BidRepository;
+import depaul.edu.FreightBid.model.Carrier;
+import depaul.edu.FreightBid.model.CarrierRepository;
+import depaul.edu.FreightBid.model.Lane;
+import depaul.edu.FreightBid.model.LaneRepository;
 
 @Service 
 public class BidService {
 	
 	@Autowired
 	BidRepository bidRepo;
+	
+	@Autowired
+	CarrierRepository carrierRepo;
+	
+	@Autowired
+	LaneRepository laneRepo;
+	
+	
+	public Bid createBid(Long carrierID, Long laneID, Bid bid) {
+		Carrier carrier = carrierRepo.findById(carrierID).get();
+		bid.setCarrier(carrier);
+		Lane lane = laneRepo.findById(laneID).get();
+		bid.setLane(lane);
+		
+		return bidRepo.save(bid);	
+		
+	}
+	
 	
 	
 	public List<Bid> getBids(){
@@ -36,9 +58,19 @@ public class BidService {
 	}
 	
 	public Bid addBid (Bid bid) {
+		
+		
+		
 		Bid newBid = bidRepo.save(bid);
 		
 		return newBid;
+	}
+	
+	public List<Bid> getBidByLane(Lane lane){
+		List <Bid> bids = new ArrayList<Bid>();
+		bidRepo.findByLane(lane).forEach(bids::add);
+		
+		return bids;
 	}
 	
 }
